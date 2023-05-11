1573de581b6a05d912dd308384f945c7ef59c261
DaniloNick <daniloageu@gmail.com>
2023-05-11 09:27:10 -0300

    Domain model

--- src/main/java/com/intensivospring/dslist/controllers/GameController.java
+++ src/main/java/com/intensivospring/dslist/controllers/GameController.java
@@ -1,7 +1,6 @@
 package com.intensivospring.dslist.controllers;
 
 import com.intensivospring.dslist.dto.GameMinDTO;
-import com.intensivospring.dslist.entities.Game;
 import com.intensivospring.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
